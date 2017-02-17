81b9e771002f8a5618d19bdb019ab8a211cf7f4c
phzean <tsingmu_qmy@126.com>
2017-02-17 12:19:48 +0800

    Initial Commit

---
+++ PZCollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PZCollectionViewDemo
+//
+//  Created by qmy on 2017/2/17.
+//  Copyright © 2017年 qmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PZCollectionViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PZCollectionViewDemo
+//
+//  Created by qmy on 2017/2/17.
+//  Copyright © 2017年 qmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
